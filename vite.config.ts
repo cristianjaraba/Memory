35761c01845c99cd0fb4c833a20898f373a72b81
Cristian Jaraba <cristianjaraba@hotmail.com>
2026-09-10 09:55:09 +0200

    feat: add initial Memory card prototype

---
+++ vite.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: "/dist/"
+});
+
+// Website: adresse.de/Memory/
+// Deshalb: base: '/Memory/'
+// Inhalt von dist auf den FTP-Ordner Memory hochladen.
